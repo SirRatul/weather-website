b598efc3e12ead22ceb76ab3501615a04ad5db53
Samsul Ratul <samsulratul98@gmail.com>
2020-03-19 22:52:40 +0600

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback)  => {
         // const url = 'https://api.darksky.net/forecast/cc4784912846f9be89cffcce6ce4d1b9/37.8267,-122.4233?lang=es'
         // const url = 'https://api.darksky.net/forecast/cc4784912846f9be89cffcce6ce4d1b9/37.8267,-122.4233?lang=es&units=si'
         //const url = 'https://api.darksky.net/forecast/cc4784912846f9be89cffcce6ce4d1b9/37.8267,-122.4233'
-        const url = 'https://api.darksky.net/forecast/cc4784912846f9be89cffcce6ce4d1b9/'+latitude+','+longitude
+        const url = 'https://api.darksky.net/forecast/cc4784912846f9be89cffcce6ce4d1b9/'+latitude+','+longitude+'?units=si'
 
         /* request({ url: url, json: true}, (error, response) => {
             if(error){
@@ -38,7 +38,7 @@ const forecast = (latitude, longitude, callback)  => {
             } else if(body.error){
                 callback('Unable to find location',undefined)
             } else {
-                callback(undefined, body.daily.data[0].summary+' It is currently '+body.currently.temperature+' degrees out. There is a '+body.currently.precipProbability+'% chance of rain.')
+                callback(undefined, body.daily.data[0].summary+' It is currently '+body.currently.temperature+' degrees out. This high today is '+body.daily.data[0].temperatureHigh+' with a low of '+body.daily.data[0].temperatureLow+'. There is a '+body.currently.precipProbability+'% chance of rain.')
             }
         })
     }, 2000)
